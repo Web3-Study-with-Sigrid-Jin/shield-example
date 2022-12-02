0170bd04de33083fcdd3405f740e956df8d42843
Sigrid <hophfg@gmail.com>
2022-12-02 17:36:58 +0900

    init

---
+++ contracts/interfaces/IMultiplierVerifier.sol
@@ -0,0 +1,13 @@
+
+    //  SPDX-License-Identifier: GPL-3.0-only
+
+    pragma solidity ^0.8.0;
+
+     interface IMultiplierVerifier {
+         function verifyProof(
+             uint256[2] calldata a,
+             uint256[2][2] calldata b,
+             uint256[2] calldata c,
+             uint[1] memory input
+         ) external view returns (bool);
+    }
